fb495bfb7c52f6aead270a2e0166f8d5b3447c39
Jonathan Hasbun <jhasbun@pdx.edu>
2016-01-24 12:42:42 -0800

    Android base project

--- app/src/main/java/com/project/game/boggle/OnePlayer.java
+++ app/src/main/java/com/project/game/boggle/OnePlayer.java
@@ -1,11 +1,13 @@
 package com.project.game.boggle;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class OnePlayer extends AppCompatActivity {
 
@@ -13,9 +15,22 @@ public class OnePlayer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_one_player);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        new CountDownTimer(180000, 1000) {
+            TextView timerTextField = (TextView) findViewById(R.id.countdown_timer);
+
+            public void onTick(long millisUntilFinished) {
+                timerTextField.setText("Timer: " + (millisUntilFinished / 60000) + ":" + ((millisUntilFinished / 1000) % 60));
+            }
+
+            /* TODO stop game and evaluate score */
+            public void onFinish() {
+                timerTextField.setText("done!");
+            }
+        }.start();
     }
 
+    public void onSubmit() {
+        
+    }
 }
--- app/src/main/java/com/project/game/boggle/TitleScreen.java
+++ app/src/main/java/com/project/game/boggle/TitleScreen.java
@@ -2,6 +2,7 @@ package com.project.game.boggle;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +10,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class TitleScreen extends AppCompatActivity {
 
