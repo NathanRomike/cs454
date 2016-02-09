7b869d8306432c0c9a7fd15744d40c5548b23f73
Nan <latree88@gmail.com>
2016-02-09 10:19:41 -0800

    Start to work on the highscore layout. It can display the top 10 highscore, but haven't test yet

--- app/src/main/java/com/project/game/boggle/Highscores.java
+++ app/src/main/java/com/project/game/boggle/Highscores.java
@@ -6,8 +6,10 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Map;
 
 public class Highscores extends AppCompatActivity {
@@ -21,8 +23,27 @@ public class Highscores extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Container container = Container.getInstance();
-        container.setHighscoresDic(container.getUser(), container.getPlayerScore());
-        container.updateHighscores(container.getHighscoresDic());
+        int length = container.getHighscores().size();
+
+        for (int i = 1; i < length + 1; i++) {
+            int j = i + 10;
+            String playerTextViewID = "highscoreTextView"+ i;
+            String scoreTextViewID = "highscoreTextView" + j;
+            int playerID = getResources().getIdentifier(playerTextViewID, "id", getPackageName());
+            int scoreID = getResources().getIdentifier(scoreTextViewID, "id", getPackageName());
+
+            if (playerID != 0) {
+                TextView playerTextView = (TextView) findViewById(playerID);
+                TextView scoreTextView = (TextView) findViewById(scoreID);
+
+
+                Iterator listIndex = container.getHighscores().get(i).keySet().iterator();
+                String listKey = (String)listIndex.next();
+                playerTextView.setText(listKey);
+                scoreTextView.setText(container.getHighscores().get(i).get(listKey));
+
+            }
+        }
     }
 
 }
