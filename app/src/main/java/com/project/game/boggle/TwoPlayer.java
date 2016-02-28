58bebdd98a40d85e34cc2f9250a07375efa5d2ea
Nan <latree88@gmail.com>
2016-02-28 15:35:00 -0800

    switch using bluetooth instead of wifi direct

--- app/src/main/java/com/project/game/boggle/TwoPlayer.java
+++ app/src/main/java/com/project/game/boggle/TwoPlayer.java
@@ -1,23 +1,47 @@
 package com.project.game.boggle;
 
+import android.content.Context;
 import android.net.nsd.NsdServiceInfo;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class TwoPlayer extends AppCompatActivity {
+//import android.content.IntentFilter;
+//import android.net.wifi.p2p.WifiP2pManager;
+//import android.net.wifi.p2p.WifiP2pManager.Channel;
+//import android.content.BroadcastReceiver;
+//import android.util.Log;
+//import java.util.ArrayList;
+//import java.util.List;
+//import android.net.wifi.p2p.WifiP2pManager.PeerListListener;
+//import android.net.wifi.p2p.WifiP2pDeviceList;
 
+import android.net.wifi.p2p.WifiP2pConfig;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.WifiP2pManager.ActionListener;
+import android.net.wifi.p2p.WifiP2pManager.Channel;
+import android.content.BroadcastReceiver;
+import android.content.IntentFilter;
+import android.util.Log;
+import android.view.Menu;
+import android.widget.Toast;
+
+
+public class TwoPlayer extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_two_player);
+
+
     }
 
     public void registerService(int port) {
         // Create the NsdServiceInfo object, and populate it.
         NsdServiceInfo serviceInfo = new NsdServiceInfo();
     }
+
+
+
 }
